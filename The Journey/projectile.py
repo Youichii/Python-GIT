49db4c1e3949f167e6ac057d5b916bdc290661c1
noellekh <63249943+noellekh@users.noreply.github.com>
2020-12-10 17:48:32 +0100

    Update projectile.py

--- The Journey/projectile.py
+++ The Journey/projectile.py
@@ -2,18 +2,18 @@ import pygame
 
 #definir la classe qui va gerer le projectile du joueur
 
-class Projectile(pygame.sprite.Sprite):
+class Projectile(pygame.sprite.Sprite): #héritage qui nous permet de prendre la classe comment élément graphique du jeu
 
     #definir le ocnstructeur
     def __init__(self, player):
         super().__init__()
         self.velocity = 5
         self.player = player
-        self.image = pygame.image.load('assets/projectile.png')
+        self.image = pygame.image.load('im/laser.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = player.rect.x + 120
-        self.rect.y = player.rect.y + 80
+        self.rect.x = player.rect.x + 300
+        self.rect.y = player.rect.y + 0
         self.origin_image = self.image
         self.angle = 0
 
@@ -32,6 +32,13 @@ class Projectile(pygame.sprite.Sprite):
     def move(self):
         self.rect.x += self.velocity
         self.rotate()
+
+        #verifier si le projectile entre en collision avec le monstre
+        for monster in self.player.game.check_collision(self, self.player.game.all_monsters):
+            #supprimer le projectile
+            self.remove()
+            # infliger des degats
+            monster.damage(self.player.attack) #le monstre subit des degats selon les attacks
 
         #verifier si notre projectile n'est plus present sur l'ecran
         if self.rect.x > 1080:
