12deb130001d190ad4d682fc571442b5b2681884
noellekh <63249943+noellekh@users.noreply.github.com>
2020-12-10 17:49:02 +0100

    Update player.py

--- The Journey/player.py
+++ The Journey/player.py
@@ -4,17 +4,19 @@ from projectile import Projectile
 #creer un class = joueur
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
+
         super().__init__()
+        self.game = game
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
         self.all_projectile = pygame.sprite.Group()
-        self.image = pygame.image.load('assets/player.png')
+        self.image = pygame.image.load('im/tank-2.png')
         self.rect = self.image.get_rect()
         self.rect.x = 400
-        self.rect.y = 300
+        self.rect.y = 450
 
     def launch_projectile(self):
 
@@ -22,7 +24,10 @@ class Player(pygame.sprite.Sprite):
 
 
     def move_right(self):
-        self.rect.x += self.velocity
+        #si le joueuer n'est pas en collision avec un monstre
+        if not self.game.check_collision(self, self.game.all_monsters):
+            self.rect.x += self.velocity
+
 
     def move_left(self):
         self.rect.x -= self.velocity
