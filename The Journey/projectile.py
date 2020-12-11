8436a98c258756ef0d8f9b851ba4ee3e230f8cc8
noellekh <63249943+noellekh@users.noreply.github.com>
2020-12-11 17:13:03 +0100

    Update projectile.py

--- The Journey/projectile.py
+++ The Journey/projectile.py
@@ -12,7 +12,7 @@ class Projectile(pygame.sprite.Sprite): #héritage qui nous permet de prendre la
         self.image = pygame.image.load('im/laser.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = player.rect.x + 300
+        self.rect.x = player.rect.x + 100
         self.rect.y = player.rect.y + 0
         self.origin_image = self.image
         self.angle = 0
