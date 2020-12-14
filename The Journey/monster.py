38b4481e9fa0101b95393f73f40ed94f900f5f23
noellekh <63249943+noellekh@users.noreply.github.com>
2020-12-14 14:15:14 +0100

    Update monster.py

--- The Journey/monster.py
+++ The Journey/monster.py
@@ -30,6 +30,7 @@ class Monster(pygame.sprite.Sprite):
             self.rect.x = 1000 # + random.randint(0, 300)
             # self.velocity = random.randint(1, 3)
             self.health = self.max_health
+            self.game.score.points +=1
 
     def update_health_bar(self, surface):
 
